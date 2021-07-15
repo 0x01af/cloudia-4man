8700fedcc343567a820fa61eaebd552543ec5a0e
0x01af <oso@osoz.ch>
2021-07-15 21:10:58 +0200

    Final version

--- rpi-configs/snarlk3s1/main.tf
+++ rpi-configs/snarlk3s1/main.tf
@@ -4,17 +4,15 @@
 # Provisioning Services by default run only at resource creation, additional runs without cleanup may introduce problems.
 # https://www.terraform.io/docs/provisioners/index.html
 
-# provider "" { }
-
 module "rpi_basic" {
   source = "../../tf-modules/rpi_basic"
   
-  rpi_ip4_temporary = "192.168.123.Y"
+  rpi_ip4_temporary = "192.168.123.118"
   
-  rpi_hostname = "hostname-new"
-  rpi_ip4 = "192.168.123.X/27"
-  rpi_ip6 = "2a02:169:67f5:1af::X/64"
-
+  rpi_hostname = "snarlk3s1"
+  rpi_ip4 = "192.168.123.21/25"
+  rpi_ip6 = "2a02:169:67f5:1af::21/64"
+  
 }
 
 # module "rpi_k3s_master" {
--- tf-modules/rpi_basic/main.tf
+++ tf-modules/rpi_basic/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    null = {
+      source = "hashicorp/null"
+      version = "3.1.0"
+    }
+  }
+}
 
 resource "null_resource" "rpi_basic" {
   connection {
@@ -8,7 +16,7 @@ resource "null_resource" "rpi_basic" {
   }
   
   provisioner "file" {
-    content     = templatefile("${path.module}/netplan.tpl", ${var})
+    content     = templatefile("${path.module}/netplan.tpl", {})
     destination = "/etc/netplan/99_config.yaml"
   }
   
@@ -16,27 +24,27 @@ resource "null_resource" "rpi_basic" {
     inline = [
       # SET HOSTNAME
       "sudo hostnamectl set-hostname ${var.rpi_hostname}",
-	  
-	  # SET FQDN IN /etc/host
+      
+      # SET FQDN IN /etc/host
       "echo '127.0.1.1 ${var.rpi_hostname}.${var.rpi_dns_domain}' | sudo tee -a /etc/hosts",
-     
+      
       # SET TIMEZONE
       "sudo timedatectl set-timezone Europe/Zurich",
-	  
-	  # SET NTP SERVER
-	  # based on: https://superuser.com/questions/723441/how-to-replace-line-in-file-with-pattern-with-sed
-	  "sudo sed -i 's/NTP=.*/NTP=snarlrtr.mesh.local/' /etc/systemd/timesyncd.conf",
-	  "sudo sed -i 's/FallbackNTP=.*/FallbackNTP=192.168.123.1/' /etc/systemd/timesyncd.conf",
+      
+      # SET NTP SERVER
+      # based on: https://superuser.com/questions/723441/how-to-replace-line-in-file-with-pattern-with-sed
+      "sudo sed -i 's/NTP=.*/NTP=snarlrtr.mesh.local/' /etc/systemd/timesyncd.conf",
+      "sudo sed -i 's/FallbackNTP=.*/FallbackNTP=192.168.123.1/' /etc/systemd/timesyncd.conf",
       
       # SYSTEM AND PACKAGE UPDATES
       "sudo apt-get update -y",
       "sudo apt-get upgrade -y",
       "sudo apt-get dist-upgrade -y",
       "sudo apt --fix-broken install -y",
-
+      
       # OPTIMIZE GPU MEMORY
       "echo 'gpu_mem=16' | sudo tee -a /boot/config.txt",
-
+      
       # REBOOT
       # Changed from 'sudo reboot' to 'sudo shutdown -r +0' to address exit status issue encountered
       # after Terraform 0.11.3, see https://github.com/hashicorp/terraform/issues/17844
