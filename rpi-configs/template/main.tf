ba5ab1bf73f24e1188ed795a9db134f0ab7a044f
0x01af <oso@osoz.ch>
2021-07-16 14:06:14 +0200

    Added Modul rpi_k3s_server to Template-Config

--- rpi-configs/template/main.tf
+++ rpi-configs/template/main.tf
@@ -13,11 +13,19 @@ module "rpi_basic" {
   rpi_ip4_temporary = "192.168.123.Y"
   
   rpi_hostname = "hostname-new"
-  rpi_ip4 = "192.168.123.X/27"
+  rpi_ip4 = "192.168.123.X/25"
   rpi_ip6 = "2a02:169:67f5:1af::X/64"
   
 }
 
-# module "rpi_k3s_master" {
-#  source = "../../tf-modules/rpi_k3s_master"
-#}
+module "rpi_k3s_server" {
+  source = "../../tf-modules/rpi_k3s_server"
+  
+  su_username = "${var.su_username}"
+  su_password = "${var.su_password}"
+  
+  rpi_ip4 = "192.168.123.X/25"
+  
+  k3s_token = "k3s-cluster-1"
+  k3s_data_dir = "/mnt/nvme1/k3s/data"
+}
