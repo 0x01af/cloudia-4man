32df169251481b41fbadaadf9b669562e5552217
Olaf <77020944+0x01af@users.noreply.github.com>
2021-08-09 21:47:03 +0200

    backend-terraform: simplify module

--- backend/terraform/os_linux/main.tf
+++ backend/terraform/os_linux/main.tf
@@ -7,6 +7,28 @@ terraform {
   }
 }
 
+# Variables
+variable "su_username" {
+  description = "Superuser's Username"
+  type        = string
+  sensitive   = true
+}
+variable "su_password" {
+  description = "Superuser's Password"
+  type        = string
+  sensitive   = true
+}
+variable "ip4_init" {
+  description = "IPv4 address, that has been initially assigned by DHCP."
+  type        = string
+}
+
+variable "hostname" {
+  description = "Hostname of the machine"
+  type        = string
+}
+
+# Module
 resource "null_resource" "os_linux" {
   connection {
     type = "ssh"
