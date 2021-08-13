e8ae198bb5206bef8a67e9ca9030f25f087d9662
Olaf <77020944+0x01af@users.noreply.github.com>
2021-08-13 15:53:58 +0200

    backend-terraform: raspberrypi_4_device - upd main

--- backend/terraform/raspberrypi_4_device/main.tf
+++ backend/terraform/raspberrypi_4_device/main.tf
@@ -29,15 +29,18 @@ variable "hostname" {
 }
 
 # Module
-resource "null_resource" "os_linux_configure_hostname" {
+resource "null_resource" "raspberrypi_4_device" {
   connection {
     type = "ssh"
     user = "${var.su_username}"
     password = "${var.su_password}"
     host = "${var.ip4_init}"
-  }  
+  }
+  
   provisioner "remote-exec" {
     inline = [
+      # OPTIMIZE GPU MEMORY
+      "echo 'gpu_mem=16' | sudo tee -a /boot/config.txt",
       # UPDATE /ETC/HOSTS
       "echo '127.0.0.1 ${var.hostname}' | sudo tee -a /etc/hosts",
       # SET HOSTNAME
