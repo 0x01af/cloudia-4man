fe41530161188cb827b5ac9a5273677003a8a4c0
Olaf <77020944+0x01af@users.noreply.github.com>
2022-07-01 22:11:59 +0200

    Upd c4m main

--- cloudia-4man.sh
+++ cloudia-4man.sh
@@ -2,17 +2,15 @@
 #
 # Contact: github.com/0x01af
 # License: need to be improved
-# Version: v2021-delta
+# Version: v2022-delta
 
 # Configuration
 C4M_CONFIG = {
- terraform_version: x.y,
  ansible_version: x.y,
  inventory_path: './inventory'
 }
 
 # Checking prerequisite
-# - terraform: if not existing, should it install it
 # - ansible: if not existing, should it install it
 
 # NOT NEEDED: Detecting new infrastructure component
@@ -26,24 +24,6 @@ C4M_CONFIG = {
 # INCLUDED AT ANSIBLE: Running provisioning service
 mkdir /inventory/{$environment}/states/{$server}
 cd /inventory/{$environment}/states/{$server}
-# - write terraform file "main.tf"
-# Content:
-module "os_linux" {
-  source = "../../../../backend/terraform/os_linux"
-  
-  su_username = "${var.su_username}"
-  su_password = "${var.su_password}"
-  
-  ip4_init = "{$ip4_init}"
-  
-  hostname = "{$server}"
-}
-#
-
-terraform init -input=false
-terraform plan -var "su_username=$username" -var "su_password=$password" -out=$server.tfplan -input=false
-terraform apply -input=false $server.tfplan
-
 
 # Starting provisioning service, and configuration & deployment management
 ansible-playbook backend/ansible/playbook-main.yml -i inventory/{$environment}/environment.yaml
