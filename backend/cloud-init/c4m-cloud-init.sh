d02ca2d931fe189a17c96b6676ea5afa290e03bf
Olaf <77020944+0x01af@users.noreply.github.com>
2026-04-23 20:58:52 +0200

    Update c4m-cloud-init.sh

--- backend/cloud-init/c4m-cloud-init.sh
+++ backend/cloud-init/c4m-cloud-init.sh
@@ -5,18 +5,16 @@ export ORIG_ISO="ubuntu-22.04.1-live-server-amd64.iso"
 export MODDED_ISO="${ORIG_ISO::-4}-c4m.iso"
 
 mkdir /tmp/c4m-tmp
-chdir /tmp/c4m-tmp
 
 # Download iso image
 sudo su -
-wget $ISO
+wget -P /tmp/c4m-tmp $ISO
 
-# Mount iso image
+# Mount iso image and copy original grub.cfg
 mkdir /tmp/c4m-tmp/iso-mnt
-mount -o loop $ORIG_ISO iso-mnt
-
-# Copy original grub.cfg
-cp --no-preserve=all iso-mnt/boot/grub/grub.cfg /tmp/c4m-tmp/grub.cfg
+mount -o loop /tmp/c4m-tmp/$ORIG_ISO /tmp/c4m-tmp/iso-mnt
+cp --no-preserve=all /tmp/c4m-tmp/iso-mnt/boot/grub/grub.cfg /tmp/c4m-tmp/grub.cfg
+umount /tmp/c4m-tmp/iso-mnt
 
 # Change grub.cfg
 sed -i 's/linux	\/casper\/vmlinuz  ---/linux	\/casper\/vmlinuz autoinstall quiet ---/g' /tmp/c4m-tmp/grub.cfg
@@ -24,3 +22,4 @@ sed -i 's/linux	\/casper\/vmlinuz  ---/linux	\/casper\/vmlinuz autoinstall quiet
 # Modify and create new iso image
 chdir ~/livefs-edit
 livefs-edit /tmp/c4m-tmp/$ORIG_ISO /tmp/c4m-tmp/$MODDED_ISO --cp /tmp/c4m-tmp/grub.cfg new/iso/boot/grub/grub.cfg
+
