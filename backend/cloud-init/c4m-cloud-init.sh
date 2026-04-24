a88e6f6ee016e6985e5355bb5c3c689822f33433
Olaf <77020944+0x01af@users.noreply.github.com>
2026-04-24 10:41:27 +0200

    Update c4m-cloud-init.sh

--- backend/cloud-init/c4m-cloud-init.sh
+++ backend/cloud-init/c4m-cloud-init.sh
@@ -21,4 +21,5 @@ bsdtar -C /tmp/c4m-tmp/iso -xf /tmp/c4m-tmp/$ORIG_ISO
 sed -i -E 's|/casper/vmlinuz\s+---|/casper/vmlinuz autoinstall quiet ---|' /tmp/c4m-tmp/iso/boot/grub/grub.cfg
 
 # Create new iso image
-xorriso -as mkisofs --modification-date='${TIMESTAMP}' --grub2-mbr --interval:local_fs:0s-15s:zero_mbrpt,zero_gpt:'${ORIG_ISO}' --protective-msdos-label -partition_cyl_align off -partition_offset 16 --mbr-force-bootable -append_partition 2 28732ac11ff8d211ba4b00a0c93ec93b --interval:local_fs:2470124d-2478587d::'ubuntu.iso' -part_like_isohybrid -iso_mbr_part_type a2a0d0ebe5b9334487c068b6b72699c7 -c '/boot.catalog' -b '/boot/grub/i386-pc/eltorito.img' -no-emul-boot -boot-load-size 4 -boot-info-table --grub2-boot-info -eltorito-alt-boot -e '--interval:appended_partition_2_start_617531s_size_8464d:all::' -no-emul-boot -boot-load-size 8464 -isohybrid-gpt-basdat -o /tmp/c4m-tmp/$MODDED_ISO -V 'c4m - ${ORIG_ISO}' /tmp/c4m-tmp/iso/
+export MKISOFS_ARGS=$(xorriso -indev /tmp/c4m-tmp/$ORIG_ISO -report_el_torito as_mkisofs 2>&1 | awk "/Volume id[[:space:]]*:[[:space:]]*'Ubuntu-Server/ {flag=1; next} flag {printf \"%s \", \$0}")
+xorriso -as mkisofs $MKISOFS_ARGS -isohybrid-gpt-basdat -o /tmp/c4m-tmp/$MODDED_ISO /tmp/c4m-tmp/iso/
